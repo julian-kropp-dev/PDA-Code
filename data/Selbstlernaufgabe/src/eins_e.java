e67b978a07fadc4b6eb66937e477e3e1cb337c41
julian-kropp-dev-MacBook <77173321+julian-kropp-dev@users.noreply.github.com>
2021-10-15 21:27:13 +0200

    created code for task 1e)

--- data/Selbstlernaufgabe/src/eins_e.java
+++ data/Selbstlernaufgabe/src/eins_e.java
@@ -1,3 +1,15 @@
 //Schreiben Sie ein Programm in Java, das die Fakultät zu einer gegebenen Zahl a berechnet.
 public class eins_e {
+    public static void main(String[] args){
+        int a = 5;
+        int fakultaet = 1;
+        int durchlaeufe = 1;
+        while (durchlaeufe <= a){
+            fakultaet = fakultaet*durchlaeufe;
+            durchlaeufe += 1;
+            System.out.println(fakultaet);
+
+        }
+    }
 }
+// 5*4*3*2*1 = 20*3*2*1 = 60*2*1= 120*1 = 120
