86f10dcc540640104639d5f35a3476f7a1c4394d
julian-kropp-dev-MacBook <77173321+julian-kropp-dev@users.noreply.github.com>
2021-10-15 15:52:26 +0200

    added task as a comment

--- data/Selbstlernaufgabe/src/eins_d.java
+++ data/Selbstlernaufgabe/src/eins_d.java
@@ -1,2 +1,5 @@
+//Schreiben Sie ein Programm in Java, das
+//a) mit zwei beliebigen Zahlen a und b beginnt, und dann
+//b) so lange a zu sich selbst dazu addiert, wie a (ohne Rest) durch b teilbar ist
 public class eins_d {
 }
