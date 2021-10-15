9ab37d67061c3237002cce901335c3beb32445a0
julian-kropp-dev-MacBook <77173321+julian-kropp-dev@users.noreply.github.com>
2021-10-15 17:32:38 +0200

    created code for task 1d)

--- data/Selbstlernaufgabe/src/eins_d.java
+++ data/Selbstlernaufgabe/src/eins_d.java
@@ -2,4 +2,16 @@
 //a) mit zwei beliebigen Zahlen a und b beginnt, und dann
 //b) so lange a zu sich selbst dazu addiert, wie a (ohne Rest) durch b teilbar ist
 public class eins_d {
+    public static void main(String[] args){
+        int a = 20;
+        int b = 5;
+        int durchlaeufe = 0;
+        while(a % b == 0){
+            a += a;
+            b +=a;
+            durchlaeufe += 1;
+        }
+        System.out.println(durchlaeufe);
+
+    }
 }
