a0119f9b98546171f5fabbf3c4dd71a40dd91b43
julian-kropp-dev-MacBook <77173321+julian-kropp-dev@users.noreply.github.com>
2021-10-15 15:46:58 +0200

    created code for task 1c)

--- data/Selbstlernaufgabe/src/eins_c.java
+++ data/Selbstlernaufgabe/src/eins_c.java
@@ -1,2 +1,17 @@
 public class eins_c {
+    public static void main(String[] args){
+        int zahl = 255;
+        int ergebnis = 0;
+
+        while (zahl >= 9){
+            if (zahl % 2 == 0){
+                ergebnis = zahl +ergebnis;
+                zahl -= 1;
+                System.out.println(ergebnis);
+            }
+            else{
+                zahl -=1;
+            }
+        }
+    }
 }
