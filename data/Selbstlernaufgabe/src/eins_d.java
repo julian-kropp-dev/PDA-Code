c41324bdaa03f94e7512bd2db362100d8c37e47a
julian-kropp-dev-MacBook <77173321+julian-kropp-dev@users.noreply.github.com>
2021-10-22 10:32:56 +0200

    improved code for task 1d)

--- data/Selbstlernaufgabe/src/eins_d.java
+++ data/Selbstlernaufgabe/src/eins_d.java
@@ -3,13 +3,14 @@
 //b) so lange a zu sich selbst dazu addiert, wie a (ohne Rest) durch b teilbar ist
 public class eins_d {
     public static void main(String[] args){
-        int a = 20;
-        int b = 5;
+        int a = 12;
+        int b = 6;
         int durchlaeufe = 0;
         while(a % b == 0){
             a += a;
-            b +=a;
             durchlaeufe += 1;
+            System.out.println(a);
+            System.out.println(durchlaeufe);
         }
         System.out.println(durchlaeufe);
 
