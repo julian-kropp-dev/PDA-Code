7395a45dfd0572e167d8fcedb73cd4cd38ddd091
julian-kropp-dev-MacBook <77173321+julian-kropp-dev@users.noreply.github.com>
2021-10-28 13:16:21 +0200

    created code for additional task 2_1

--- data/Selbstlernaufgabe/src/Zwei_Eins.java
+++ data/Selbstlernaufgabe/src/Zwei_Eins.java
@@ -1,2 +1,12 @@
+//converter celsius --> fahrenheit & fahrenheit --> celsius
+import java.util.Scanner;
 public class Zwei_Eins {
+    public static void main(String[] args) {
+        Scanner user_input = new Scanner(System.in);
+        System.out.println("Celsius:");
+        float celsius = user_input.nextFloat();
+        float fahrenheit = (celsius * 9) / 5 + 32;
+        System.out.println("Fahrenheit = " + fahrenheit);
+        user_input.close();
+    }
 }
