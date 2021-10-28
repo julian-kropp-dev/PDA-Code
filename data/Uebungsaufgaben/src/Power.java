f2f889e710d5fa0f62a7de6b5d1b5412806a4f12
julian-kropp-dev-MacBook <77173321+julian-kropp-dev@users.noreply.github.com>
2021-10-28 13:15:20 +0200

    improved code for Power.java

--- data/Uebungsaufgaben/src/Power.java
+++ data/Uebungsaufgaben/src/Power.java
@@ -2,9 +2,9 @@ public class Power { // corrected "Class" to "class"
     public static void main(String[] args){
         int a = 2; // renamed all integer to int and added semicolons (;)
         int b = 4;
-        // calculate a to the power of b (a^b)
+        // calculated a to the power of b (a^b)
         int result = 1; // corrected "integer" to "int" and result value has to be 1 not 0
-        while (b != 0){ // "repeat" does not exist, only while, in a while loop you don't need to classify the varibale name
+        while (b != 0){ // "repeat" does not exist, only while, in a while loop you don't need to classify the variable name
             result *= a;
             b -= 1;
         }
