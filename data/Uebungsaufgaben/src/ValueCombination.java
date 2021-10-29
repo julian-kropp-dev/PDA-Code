d101822789e610c6191288c33c0ff2fa1f2eaa79
julian-kropp-dev-MacBook <77173321+julian-kropp-dev@users.noreply.github.com>
2021-10-29 23:05:16 +0200

    created code for task 02_02

---
+++ data/Uebungsaufgaben/src/ValueCombination.java
@@ -0,0 +1,16 @@
+public class ValueCombination {
+    public static void main(String[] args) {
+        int i = 5;
+        long l = 10L;
+        float f = 15.0F;
+        double d = 20.0D;
+        char c = 'c';
+
+        double sumDouble = i + l + f + d + c; // char c's value is 99.0 so the System.out.println(sumDouble) is 149.0
+        System.out.println(sumDouble);
+
+        int sumInt = (int) (i + l + f + d + c); // I don't understand my own line of code lol
+        System.out.println(sumDouble);
+
+    }
+}
