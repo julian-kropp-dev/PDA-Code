7b178a34caaafdeb8dfa8fb17ae45d668ac2ced4
julian-kropp-dev-MacBook <77173321+julian-kropp-dev@users.noreply.github.com>
2021-10-29 23:05:30 +0200

    created code for task 02_01

---
+++ data/Uebungsaufgaben/src/StarDistance.java
@@ -0,0 +1,8 @@
+public class StarDistance {
+    public static void main(String[] args) {
+        long d1 = 8000000000000000000L;
+        long d12 = 3000000000000000000L;
+        long d2 = d1 + d12;
+        System.out.println(d2);
+    }
+}
