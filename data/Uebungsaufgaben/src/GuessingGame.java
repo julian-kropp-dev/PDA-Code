2b11f6a8e947ca57b3491fee715fd6fc06739dca
julian-kropp-dev-MacBook <77173321+julian-kropp-dev@users.noreply.github.com>
2021-10-30 17:19:31 +0200

    created code for task 02_04

---
+++ data/Uebungsaufgaben/src/GuessingGame.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class GuessingGame {
+    public static void main(String[] args) {
+        int randomNumber = (int) (Math.random() * 101.0);
+        int guesses = 0;
+        boolean guess = false;
+        while (!guess){
+            Scanner userInput = new Scanner(System.in);
+            System.out.println("Zahl:");
+            int usersGuess = userInput.nextInt();
+            if (usersGuess > randomNumber){
+                System.out.println("Zufallszahl ist kleiner!");
+                guesses += 1;
+            }
+            else if (usersGuess < randomNumber){
+                System.out.println("Zufallszahl ist groesser!");
+                guesses += 1;
+            }
+            else {
+                System.out.println("Geschafft! Zufallszahl war " + randomNumber);
+                System.out.println("Anzahl der Versuche: " + guesses);
+                guess = true;
+
+            }
+        }
+
+    }
+}
