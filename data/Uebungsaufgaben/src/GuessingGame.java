ec86563df15bfb0f151bbfa38c178ba16162a195
julian-kropp-dev-MacBook <77173321+julian-kropp-dev@users.noreply.github.com>
2021-11-03 20:06:18 +0100

    added comments

--- data/Uebungsaufgaben/src/GuessingGame.java
+++ data/Uebungsaufgaben/src/GuessingGame.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 public class GuessingGame {
     public static void main(String[] args) {
-        int randomNumber = (int) (Math.random() * 101.0);
-        int guesses = 0;
-        while (true){
+        int randomNumber = (int) (Math.random() * 101.0); //Math.random creates a random int between 0 and 1. *101 to convert it to a random int between 0-100
+        int guesses = 0; //users guesses
+        while (true){ // while the user's guess is incorrect, it asks the user each time for a new guess
             Scanner userInput = new Scanner(System.in);
             System.out.println("Zahl:");
             int usersGuess = userInput.nextInt();
@@ -18,7 +18,7 @@ public class GuessingGame {
             else {
                 System.out.println("Geschafft! Zufallszahl war " + randomNumber);
                 System.out.println("Anzahl der Versuche: " + guesses);
-                break;
+                break; //random number guessed, while-loop ends
 
             }
         }
