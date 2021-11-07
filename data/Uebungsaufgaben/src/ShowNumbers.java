f8f299d39967d9bcd28fa89ef2bb6909a7ae8b56
julian-kropp-dev-MacBook <77173321+julian-kropp-dev@users.noreply.github.com>
2021-11-07 13:45:17 +0100

    created code for ShowNumbers.java

---
+++ data/Uebungsaufgaben/src/ShowNumbers.java
@@ -0,0 +1,12 @@
+public class ShowNumbers {
+    public static void main(String[] args) {
+        int[] data = new int[] {1, 2, 3, 4, 2};
+        int i = 0;
+        String colon = ", ";
+        for (int j: data){
+            if (i > (data.length - 2)) colon = "";
+            System.out.print(data[(i)] + colon );
+            i ++;
+        }
+    }
+}
