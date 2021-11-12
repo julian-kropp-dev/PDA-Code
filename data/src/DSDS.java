961d3ec5ea938317ae3750b5828b0633083768c2
julian-kropp-dev-MacBook <77173321+julian-kropp-dev@users.noreply.github.com>
2021-11-12 19:22:59 +0100

    created initial code for DSDS.jav

---
+++ data/src/DSDS.java
@@ -0,0 +1,6 @@
+//Additional task: User enters a number of musicans and a number of votes for each of them. The programm shows a diagram
+public class DSDS {
+    public void main(String[] args){
+
+    }
+}
