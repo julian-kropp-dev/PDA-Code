442f0d483c2aaa2733967791bc5842b77d1eb8dd
julian-kropp-dev-MacBook <77173321+julian-kropp-dev@users.noreply.github.com>
2021-12-20 10:59:35 +0100

    learning the basics of (linked) lists

---
+++ data/Selbstlernaufgabe/src/learninglists/ListElement.java
@@ -0,0 +1,11 @@
+package learninglists;
+
+public class ListElement {
+    String s;
+    ListElement next;
+
+    public ListElement(String s){
+        this.s = s;
+        next = null;
+    }
+}
--- data/Selbstlernaufgabe/src/learninglists/Lists.java
+++ data/Selbstlernaufgabe/src/learninglists/Lists.java
@@ -1,4 +1,21 @@
 package learninglists;
 
 public class Lists {
+    ListElement start;
+
+    public Lists(){
+        start = new ListElement("Kopf");
+    }
+
+    public void insertLast(String s){
+        ListElement newElement = new ListElement(s);
+        start.next = newElement;
+    }
+    private ListElement findIndex(int n) {
+        ListElement iterator = start;
+        for(int i = 0; i < n; i++){
+            iterator = iterator.next;
+        }
+        return iterator;
+    }
 }
