0c65343b49aa7cd03dec7ccce9d1353b5ab16e1a
julian-kropp-dev-MacBook <77173321+julian-kropp-dev@users.noreply.github.com>
2022-01-03 17:55:15 +0100

    updated code

--- data/Uebungsaufgaben/src/uebung9/lists/SortedLinkedIntListTest.java
+++ data/Uebungsaufgaben/src/uebung9/lists/SortedLinkedIntListTest.java
@@ -3,33 +3,40 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class SortedLinkedIntListTest {
+    SortedLinkedIntList newlist = new SortedLinkedIntList(); //Start-Element
+
     @Test
-    public static void main(String[] args) {
-        SortedLinkedIntList newlist = new SortedLinkedIntList(); //Start-Element
-        //
+    void testCase1() {
         newlist.reset(); // reset vor getNext() und hasNext()
         assertFalse(newlist.hasNext());
-        //
+    }
+
+    @Test
+    void testCase2() {
+        newlist.add(3);
+        newlist.reset();
+        assertTrue(newlist.hasNext());
+        assertEquals(3, newlist.getNext());
+        assertFalse(newlist.hasNext());
+    }
+
+    @Test
+    void testCase3() {
         newlist.add(4);
         newlist.reset();
         assertTrue(newlist.hasNext());
         assertEquals(4, newlist.getNext());
         assertFalse(newlist.hasNext());
-        //
-        newlist.add(5);
-        newlist.reset();
-        assertTrue(newlist.hasNext());
-        assertEquals(4, newlist.getNext());
-        assertTrue(newlist.hasNext());
-        assertEquals(5, newlist.getNext());
-        assertFalse(newlist.hasNext());
-        //
+
+    }
+
+    @Test
+    void testCase4() {
         newlist.add(8);
         newlist.add(90);
         newlist.add(-2);
         newlist.add(10);
 
-        assertArrayEquals(new int[] {-2, 4, 5, 8, 10, 90}, newlist.toArray());
-
+        assertArrayEquals(new int[] {-2, 8, 10, 90}, newlist.toArray());
     }
 }
