5a745db0f5a39956c6cc9e02b6ee54ea5b9fec23
julian-kropp-dev-MacBook <77173321+julian-kropp-dev@users.noreply.github.com>
2022-01-22 17:48:16 +0100

    updated code

--- data/Uebungsaufgaben/src/uebung12/KeyValue.java
+++ data/Uebungsaufgaben/src/uebung12/KeyValue.java
@@ -66,16 +66,59 @@ class HashTableOverflow {
 }
 
 class HashTableOpen {
+    private final int listSize = 7;
+    private int versuch = 0;
+    private KeyValue[] quadraticProbing = new KeyValue[listSize];
+
+    public int hashfunction(int data) {
+        return ((int) Math.floorMod(data, listSize));
+    }
+
+    public int quadraticProbingFunc(int data) {
+        double funcS = Math.pow(Math.ceil(versuch / 2.0), 2) * Math.pow((-1), versuch);
+        int mod = (int) Math.floorMod((int) (hashfunction(data) - funcS), listSize);
+        return mod;
+    }
+
+    public boolean add(KeyValue data) {
+        int counter = 0;
+        for (int i = 0; i < quadraticProbing.length; i++) {
+            if(quadraticProbing[i] != null) {
+                counter++; //zählt belegte Plätze
+            }
+        }
+
+        if (counter < quadraticProbing.length) { //nur Objekte hinzufügen, wenn Liste noch Platz hat
+            if (quadraticProbing[quadraticProbingFunc(data.getKey())] == null) {
+                quadraticProbing[quadraticProbingFunc(data.getKey())] = data;
+            } else {
+                versuch++;
+                add(data);
+            }
+
+        }
+        return true;
+
+    }
+
+    public KeyValue get(int key) {
+        return quadraticProbing[key];
+    }
+
 
 }
 
 class HashTest {
 
-    KeyValue object1 = new KeyValue(0, "Julian");
-    KeyValue object2 = new KeyValue(1, "Frauke");
-    KeyValue object3 = new KeyValue(2, "Maik");
+    KeyValue object1 = new KeyValue(12, "12");
+    KeyValue object2 = new KeyValue(5, "5");
+    KeyValue object3 = new KeyValue(53, "53");
+    KeyValue object4 = new KeyValue(2, "2");
+    KeyValue object5 = new KeyValue(15, "15");
+    KeyValue object6 = new KeyValue(19, "19");
 
     HashTableOverflow table1 = new HashTableOverflow();
+    HashTableOpen table2 = new HashTableOpen();
 
 
 
@@ -83,6 +126,11 @@ class HashTest {
     void testHashfunktion() {
        assertEquals(6, table1.hashfunction(-1)); //Division-mit-Rest-Methode, Beispiel aus Hinweis 3
 
+    }
+
+    @Test
+    void testQuadraticProbing() {
+        assertEquals(5, table2.quadraticProbingFunc(12));
     }
 
     @Test
@@ -98,10 +146,31 @@ class HashTest {
     @Test
     void testAddQP() {
 
+
     }
 
     @Test
     void testGetQP() {
+        KeyValue[] erwartetesArray = new KeyValue[] {object6, object5, object4, null, object3, object1, object2};
+        table2.add(object1);
+        table2.add(object2);
+        table2.add(object3);
+        table2.add(object4);
+        table2.add(object5);
+        table2.add(object6);
+
+
+
+        assertEquals(erwartetesArray[0], object6);
+        assertEquals(erwartetesArray[1], object5);
+        assertEquals(erwartetesArray[2], object4);
+        assertEquals(erwartetesArray[3], null);
+        assertEquals(erwartetesArray[4], object3);
+        assertEquals(erwartetesArray[5], object1);
+        assertEquals(erwartetesArray[6], object2);
+
+
+
 
     }
 
